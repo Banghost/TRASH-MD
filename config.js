1292f732af5599a84e28672c934dbe29caca3b0a
Banghost <103867212+Banghost@users.noreply.github.com>
2022-06-07 13:46:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.owner = ['6281220670449','0']
-global.packname = 'Bot Whatsapp'
+global.owner = ['6281334728986','0']
+global.packname = 'Bot MrD'
 global.author = 'TRASH'
 global.sessionName = 'data'
 global.prefa = ['','!','.','#']
-global.ownername = 'KurrXd'
+global.ownername = 'MrD'
 global.sp = '>'
 global.mess = {
     admin: 'Fitur Khusus Admin Group!',
